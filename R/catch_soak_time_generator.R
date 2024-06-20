4fa95406e994e1ebca5239392ec98852157b480a
d2gex <daniel.garcia@d2garcia.com>
2024-06-20 10:47:41 +0200

    Start building the class to calculate gear area ratio

--- R/catch_soak_time_generator.R
+++ R/catch_soak_time_generator.R
@@ -1,4 +1,4 @@
-SpeciesCatchBySoakTimeGenerator <- R6::R6Class("SpeciesCatchBySoakTimeGenerator", public = list(
+GearAreaRatioCalculator <- R6::R6Class("GearAreaRatioCalculator", public = list(
 
   # @formatter:off
   #' @description
@@ -10,22 +10,121 @@ SpeciesCatchBySoakTimeGenerator <- R6::R6Class("SpeciesCatchBySoakTimeGenerator"
   # @formatter:on
   gear_soaktime = NULL,
   gear_area = NULL,
-  catch = NULL,
-  initialize = function(gear_soaktime, gear_area, catch) {
+  num_repetions = NULL,
+  initialize = function(gear_soaktime, gear_area, num_repetions = 1024) {
     self$gear_soaktime <- gear_soaktime
     self$gear_area <- gear_area
-    self$catch <- catch
+    self$num_repetions <- num_repetions
   },
-  get_same_gear_soaktime = function() {
+  calculate_area_ratio = function() {
+    gear_names <- self$gear_area$gear
+    for (i in 1:self$num_repetions) {
+
+    }
 
   }
+), private = list(
+  fetch_separate_gear_dataframes = function() {
+    gear_names <- self$gear_area$gear
+    gear_details <- list()
+    for (g_name in gear_names) {
+      gear_details[[g_name]] <- private$calculate_cumulative_soaktime(g_name)
+    }
+    return (gear_details)
+  },
+  # @formatter:off
+  #'@description
+  #' Calculate the cumulative soaktime for a given gear by first randomising the rows of the dataframe
+  #'
+  #' @param gear_name name of the gear
+  #' @returns dataframe
+  #' @export
+  # @formatter:on
+  calculate_cumulative_soaktime = function(gear_name) {
+    single_gear_soaktime <- self$gear_soaktime %>%
+      dplyr::filter(gear == gear_name)
 
+    # ransomise rows
+    single_gear_soaktime <- single_gear_soaktime[sample(nrow(gear_data)),]
+
+    single_gear_soaktime <- single_gear_soaktime %>%
+      dplyr::mutate(id = 1:nrow(single_gear_soaktime)) %>%
+      dplyr::mutate(soaktime_sum = cumsum(soak_time))
+    return(single_gear_soaktime)
+  },
+  # @formatter:off
+  #'@description
+  #' get the maximum soaktime of the gear that is least employed
+  #'
+  #' @param gears array of gears for which cummulative soaktime has already been calculated
+  #' @returns a single list with name of gear and max soaktime
+  #' @export
+  # @formatter:on
+  get_max_soaktime_least_used_gear = function(gears) {
+    return(which.min(lapply(gears, function(x) { max(x$soaktime_sum) })))
+  },
+  # @formatter:off
+  #'@description
+  #' Level off every gear's total soaktime to a given value
+  #'
+  #' @param gears array of gears for which cummulative soaktime has already been calculated
+  #' @param soaktime_threshold a named list containing the name and the maximum soaktime of the least used gear
+  #' @returns a named list containing the levelled-off gear dataframes
+  #' @export
+  # @formatter:on
+  cut_gears_soaktime_by_threshold = function(gears, soaktime_threshold) {
+    return(
+      lapply(gears, function(x) {
+        x <- x %>% dplyr::filter(soaktime_sum <= soaktime_threshold[[1]])
+      })
+    )
+  },
+  # @formatter:off
+  #'@description
+  #' Calculate the area ratio of each gear based on the total number of panels and the average panel's area ratio
+  #'
+  #' @param gears array of gears for which cummulative soaktime has been levelled off
+  #' @returns a named list of the area ratios
+  #' @export
+  # @formatter:on
+  calculate_gear_area_ratios = function(gears) {
+    gear_ratios <- list()
+    for (gear_name in names(gears)) {
+      num_panels <- sum(gears[[gear_name]]$panels)
+      panel_ratio <- self$gear_area['gear' == gear_name, 'area_ratio']
+      gear_ratios [[gear_name]] <- num_panels * panel_ratio
+    }
+    return(gear_ratios)
+  },
+  # @formatter:off
+  #'@description
+  #' Get the unique haul ids of each row for all gears involved in the calculation of the fishing power
+  #'
+  #' @param gears array of gears for which cummulative soaktime has been levelled off
+  #' @returns a vector with haul_ids
+  #' @export
+  # @formatter:on
+  get_involved_haul_ids = function(gears) {
+    return(
+      unique(
+        unlist(
+          unname(
+            lapply(gears, function(x) {
+              x$haul_id
+            })
+          )
+        )
+      )
+    )
+  }
 ))
 
 source("config.R")
 if (!exists('catch')) {
   catch <- readr::read_csv(DB_TALLAS_PARTIALLY_CLEAN_PATH)
 }
+
+# This bit is going to be given
 gear_soacktime <- readr::read_csv(DB_ARTES_PIEZAS_SOAK_TIME_CLEAN_PATH)
 gear_ratio_area <- (readr::read_csv(DB_ARTES_AREA_RATIO_PATH)) %>%
   dplyr::filter(gear %in% c("MIÑOS", "VETAS"))
@@ -39,7 +138,7 @@ t.luscus <- gear_soacktime %>%
 # Calculate the soaktime of each haul for minios by randomising the rows first
 t.luscus.minios <- t.luscus %>%
   dplyr::filter(gear == "MIÑOS")
-t.luscus.minios <- t.luscus.minios[sample(nrow(t.luscus)), ]
+t.luscus.minios <- t.luscus.minios[sample(nrow(t.luscus.minios)),]
 
 t.luscus.minios <- t.luscus.minios %>%
   dplyr::mutate(id = 1:nrow(t.luscus.minios)) %>%
@@ -47,10 +146,12 @@ t.luscus.minios <- t.luscus.minios %>%
 
 # Calculate the soaktime of each haul for vetas by randomising the rows first
 t.luscus.vetas <- t.luscus %>% dplyr::filter(gear == "VETAS")
+t.luscus.vetas <- t.luscus.vetas[sample(nrow(t.luscus.vetas)),]
+
 t.luscus.vetas <- t.luscus.vetas %>%
   dplyr::mutate(id = 1:nrow(t.luscus.vetas)) %>%
   dplyr::mutate(soaktime_sum = cumsum(soak_time))
-t.luscus.vetas <- t.luscus.vetas[sample(nrow(t.luscus)), ]
+
 
 # Cut the soaktime of the gears above the maximun soaktime of the gear least employed
 t.luscus.minios_cut_soaktime <- t.luscus.minios %>%
@@ -59,7 +160,7 @@ t.luscus.minios_cut_soaktime <- t.luscus.minios %>%
 # Calculate the ratio among gears' total area
 t.luscus.minios_area <- sum(t.luscus.minios_cut_soaktime$panels) * 0.58
 t.luscus.vetas_area <- sum(t.luscus.vetas$panels) * 1
-t.luscus.minios <- round(t.luscus.minios_area / t.luscus.vetas_area, 2)
+t.luscus.minios_ratio <- round(t.luscus.minios_area / t.luscus.vetas_area, 2)
 
 # Fetch the hauls that will be used to calculate the selectivity curve
 hauls_id <- unique(c(t.luscus.vetas$haul_id, t.luscus.minios_cut_soaktime$haul_id))
